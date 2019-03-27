a9d1e88c7254d3ffc8bb1258183a825558807be4
dnfka <36830518+dnfka@users.noreply.github.com>
2019-03-27 14:42:00 +0900

    add alarm log in ewpsp

--- ewpsp/src/main/java/kr/co/ewp/ewpsp/web/ApiController.java
+++ ewpsp/src/main/java/kr/co/ewp/ewpsp/web/ApiController.java
@@ -53,10 +53,10 @@ public class ApiController {
 
 	private static final Logger logger = LoggerFactory.getLogger(ApiController.class);
 	
-  @Autowired
-  private AlarmService alarmService;
-  @Autowired
-  private DeviceMonitoringService deviceMonitoringService;
+	@Autowired
+	private AlarmService alarmService;
+	@Autowired
+	private DeviceMonitoringService deviceMonitoringService;
 
 	@Resource(name="cmpyGrpSiteMngService")
 	private CmpyGrpSiteMngService cmpyGrpSiteMngService;
@@ -67,59 +67,60 @@ public class ApiController {
 	@Resource(name="loginService")
 	private LoginService loginService;
 
-  @RequestMapping(value = { "/v1/alarm" }, method = RequestMethod.POST, produces = MediaType.TEXT_HTML_VALUE) /*** 12.13 이우람 수정 ***/
-  public void getPrototype(HttpServletRequest request, HttpServletResponse response, ModelMap modelMap, //
-      @RequestParam String siteId, // 사이트ID /*** 12.13 이우람 추가 ***/
-      @RequestParam String deviceId, // 장치ID
-      @RequestParam Integer deviceType, // 장치구분(1:PCS,2:BMS,3:PV,4:부하측정기기,5:PV모니터링기기,6:ESS모니터링기기,7:iSmart,8:총량기기) /*** 12.13 이우람 수정 ***/
-      @RequestParam Long alarmTime, // 알람발생일시(형식:YYYYMMDDhhmmss) -> 기준일시에 저장 /*** 12.13 이우람 수정, 2019.03.13 이우람 수정(Long(timestamp형식으로 변경) ***/
-      @RequestParam Integer alarmType, // 알람구분(1:비상,2:주의) /*** 12.13 이우람 수정 ***/
-      @RequestParam(defaultValue = "") String alarmMsg, // 알람메시지 (파라메터는 필수이고 값은 있는 경우에만 세팅)
-      @RequestParam(required = false, defaultValue = "") String alarmCode // 알람코드
-  ) {
-    try {
-    	Map<String, Object> dvMap = Maps.newHashMap();
-    	dvMap.put("siteId", siteId);
-        dvMap.put("deviceId", deviceId);
-        dvMap.put("deviceType", deviceType);
-      Map<String, String> device = deviceMonitoringService.getDevice(dvMap);
-//      String siteId = device == null ? "" : device.get("site_id"); /*** 12.13 이우람 수정 ***/
+	@RequestMapping(value = { "/v1/alarm" }, method = RequestMethod.POST, produces = MediaType.TEXT_HTML_VALUE) /*** 12.13 이우람 수정 ***/
+	public void getPrototype(HttpServletRequest request, HttpServletResponse response, ModelMap modelMap, //
+		@RequestParam String siteId, // 사이트ID /*** 12.13 이우람 추가 ***/
+		@RequestParam String deviceId, // 장치ID
+		@RequestParam Integer deviceType, // 장치구분(1:PCS,2:BMS,3:PV,4:부하측정기기,5:PV모니터링기기,6:ESS모니터링기기,7:iSmart,8:총량기기) /*** 12.13 이우람 수정 ***/
+		@RequestParam Long alarmTime, // 알람발생일시(형식:YYYYMMDDhhmmss) -> 기준일시에 저장 /*** 12.13 이우람 수정, 2019.03.13 이우람 수정(Long(timestamp형식으로 변경) ***/
+		@RequestParam Integer alarmType, // 알람구분(1:비상,2:주의) /*** 12.13 이우람 수정 ***/
+		@RequestParam(defaultValue = "") String alarmMsg, // 알람메시지 (파라메터는 필수이고 값은 있는 경우에만 세팅)
+		@RequestParam(required = false, defaultValue = "") String alarmCode // 알람코드
+	) {
+		System.out.println("siteId : "+siteId+", deviceId : "+deviceId+", deviceType : "+deviceType+", alarmTime : "+alarmTime+", alarmType : "+alarmType+", alarmMsg : "+alarmMsg+", alarmCode : "+alarmCode);
+		try {
+			Map<String, Object> dvMap = Maps.newHashMap();
+			dvMap.put("siteId", siteId);
+			dvMap.put("deviceId", deviceId);
+			dvMap.put("deviceType", deviceType);
+			Map<String, String> device = deviceMonitoringService.getDevice(dvMap);
+//			String siteId = device == null ? "" : device.get("site_id"); /*** 12.13 이우람 수정 ***/
 
-      Map<String, Object> parameter = Maps.newHashMap();
-      parameter.put("siteId", siteId);
-      parameter.put("deviceId", deviceId);
-      parameter.put("deviceType", deviceType); // 1: PCS, 2: BMS, 3: IVT (PV), 4: AMI, 5: AMI Modem, 6: Enertalk
-      Timestamp stdDate = new Timestamp(alarmTime);
-      parameter.put("stdDate", stdDate); // event time in millisecond
-      parameter.put("alarmCode", alarmCode);
-      parameter.put("alarmType", alarmType); // 1: emergency, 2: warning
-      parameter.put("alarmMsg", alarmMsg); // alarm message
-      parameter.put("alarmCfmYn", "N");
-      parameter.put("alarmCfmDate", null);
-      parameter.put("alarmCfmUid", "");
-      parameter.put("alarmActYn", "N");
-      parameter.put("alarmNote", "");
-      alarmService.addAlarm(parameter);
-      response.setContentType("text/html; charset=UTF-8");
-      response.getWriter().print("0");
-      
-      // SMS 전송 (한재종)
-      if(device !=null){
-    	  String alarmTimeStr = CommonUtils.convertDateFormat(stdDate, "yyyyMMddHHmmss"); /*** 12.13 이우람 추가 ***/
-    	  smsService.sendAlarmMessage(siteId, deviceId, device.get("device_name"), alarmTimeStr, Integer.toString(alarmType), alarmMsg); /*** 12.13 이우람 수정 ***/
-      }
-    } catch (NullPointerException e) {
-		logger.error("error is : "+e.toString());
-    } catch (Exception e) {
-    	logger.error("error is : "+e.toString());
-      response.setContentType("text/html; charset=UTF-8");
-      try {
-        response.getWriter().print("-1");
-      } catch (IOException e1) {
-    	  logger.error("error is : "+e1.toString());
-      }
-    }
-  }
+			Map<String, Object> parameter = Maps.newHashMap();
+			parameter.put("siteId", siteId);
+			parameter.put("deviceId", deviceId);
+			parameter.put("deviceType", deviceType); // 1: PCS, 2: BMS, 3: IVT (PV), 4: AMI, 5: AMI Modem, 6: Enertalk
+			Timestamp stdDate = new Timestamp(alarmTime);
+			parameter.put("stdDate", stdDate); // event time in millisecond
+			parameter.put("alarmCode", alarmCode);
+			parameter.put("alarmType", alarmType); // 1: emergency, 2: warning
+			parameter.put("alarmMsg", alarmMsg); // alarm message
+			parameter.put("alarmCfmYn", "N");
+			parameter.put("alarmCfmDate", null);
+			parameter.put("alarmCfmUid", "");
+			parameter.put("alarmActYn", "N");
+			parameter.put("alarmNote", "");
+			alarmService.addAlarm(parameter);
+			response.setContentType("text/html; charset=UTF-8");
+			response.getWriter().print("0");
+			
+			// SMS 전송 (한재종)
+			if(device !=null){
+				String alarmTimeStr = CommonUtils.convertDateFormat(stdDate, "yyyyMMddHHmmss"); /*** 12.13 이우람 추가 ***/
+				smsService.sendAlarmMessage(siteId, deviceId, device.get("device_name"), alarmTimeStr, Integer.toString(alarmType), alarmMsg); /*** 12.13 이우람 수정 ***/
+			}
+		} catch (NullPointerException e) {
+			logger.error("error is : "+e.toString());
+		} catch (Exception e) {
+			logger.error("error is : "+e.toString());
+			response.setContentType("text/html; charset=UTF-8");
+			try {
+				response.getWriter().print("-1");
+			} catch (IOException e1) {
+				logger.error("error is : "+e1.toString());
+			}
+		}
+	}
 
 	@RequestMapping("/getSoc")
 	public @ResponseBody Map<String, Object> getSoc(@RequestParam HashMap param, HttpServletRequest request) throws Exception {
