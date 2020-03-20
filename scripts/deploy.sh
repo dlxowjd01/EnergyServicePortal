e3fc85ed680176f84f8edef7dae1b081d9057164
Duckuism <rnejrl123456@gmail.com>
2020-03-20 13:18:33 +0900

    Configure CircleCI in iDERMS-SaaS branch

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BUILD_PATH=/home/ubuntu/build/
+BUILD_PATH=/usr/local/apache-tomcat-9.0.31/webapps/
 
 echo "> 현재 실행중인 tomcat 애플리케이션 pid 확인"
 CURRENT_PID=$(pgrep -f tomcat)
@@ -8,28 +8,27 @@ if [ -z $CURRENT_PID ]
 then
   echo "> 현재 구동중인 tomcat 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  sudo service tomcat stop
+  $CATALINA_HOME/bin/shutdown.sh
   sleep 5
 fi
 
-JAR_NAME=ewp-api-0.0.1-SNAPSHOT.jar
-echo "> build 파일명: $JAR_NAME"
+#JAR_NAME=ewp-api-0.0.1-SNAPSHOT.jar
+#echo "> build 파일명: $JAR_NAME"
 
-APPLICATION_JAR_NAME=ewp-api.jar
-DEPLOY_PATH=/home/ubuntu/ewpsp_batch/lib/
-APPLICATION_JAR=$DEPLOY_PATH$APPLICATION_JAR_NAME
+#APPLICATION_JAR_NAME=ewp-api.jar
+#DEPLOY_PATH=/home/ubuntu/ewpsp_batch/lib/
+#APPLICATION_JAR=$DEPLOY_PATH$APPLICATION_JAR_NAME
 
-echo "> $APPLICATION_JAR 배포"
-cp -R $BUILD_PATH$JAR_NAME $APPLICATION_JAR
+#echo "> $APPLICATION_JAR 배포"
+#cp -R $BUILD_PATH$JAR_NAME $APPLICATION_JAR
 
 WAR_NAME=ewpsp.war
 echo "> build 파일명: $WAR_NAME"
 
-DEPLOY_PATH=/home/ubuntu/ewpsp
+DEPLOY_PATH=/usr/local/apache-tomcat-9.0.31/webapps/
 cd $DEPLOY_PATH
 
-cp -R $BUILD_PATH$WAR_NAME .
-jar -xvf $WAR_NAME
+#jar -xvf $WAR_NAME
 
 echo "> Web + API 배포"
-sudo service tomcat start
+$CATALINA_HOME/bin/startup.sh
