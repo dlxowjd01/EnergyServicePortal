838b0ca09fbc90bc30095b27d30098ac22465410
jaekeun77 <63623588+jaekeun77@users.noreply.github.com>
2021-02-15 15:15:45 +0900

    ULON-932 (#1529)
    * ULON-860
    
    2번째 Y축 표기 수정
    
    * ULON-932
    
    오류 수정

--- energyServicePortal_ewp_adv/src/main/java/kr/co/esp/common/interceptor/PreLoadInterceptor.java
+++ energyServicePortal_ewp_adv/src/main/java/kr/co/esp/common/interceptor/PreLoadInterceptor.java
@@ -208,6 +208,7 @@ public class PreLoadInterceptor extends HandlerInterceptorAdapter {
 			parameters.clear();
 			parameters.put("includeSites", "true");
 			parameters.put("includeDevices", "false");
+			parameters.put("addCapacity", "true");
 
 			Map<String, Object> userSiteGroupSearch = get("/auth/me/groups", mode, parameters, token); //그룹화되어있는 사이트 리스트 정보
 			if (200 == (int) userSiteGroupSearch.get("code")) {
